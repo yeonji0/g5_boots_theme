0b78c37c4cef7f7f1df321bede6236df011f72b0
yeonji0 <kyj0279@gmail.com>
2022-08-30 15:23:52 +0900

    d

--- boots/head.php
+++ boots/head.php
@@ -23,42 +23,48 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
 <!-- 상단 시작 { -->
     <nav class="navbar navbar-expand-sm navbar-dark bg-light">
         <div class="container">
-        <a class="navbar-brand" href="#">
-            <img src="<? echo G5_THEME_IMG_URL ?>/logo.svg" alt="" style="whidth:180px">
-        </a>
-        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-            aria-expanded="false" aria-label="Toggle navigation"></button>
-        <div class="collapse navbar-collapse" id="collapsibleNavId">
-            <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdownId">
-                        <a class="dropdown-item" href="#">Action 1</a>
-                        <a class="dropdown-item" href="#">Action 2</a>
-                    </div>
-                </li>
-            </ul>
-            <form class="d-flex my-2 my-lg-0">
-                <input class="form-control me-sm-2" type="text" placeholder="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
+            <a class="navbar-brand" href="#">
+                <img src="<? echo G5_THEME_IMG_URL ?>/logo.svg" alt="" style="whidth:180px">
+            </a>
+
+            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                aria-expanded="false" aria-label="Toggle navigation"></button>
+
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+
+                <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+
+                    <?php
+                    $menu_datas = get_menu_db(0, true);
+                    $gnb_zindex = 999; // gnb_1dli z-index 값 설정용
+                    $i = 0;
+                    foreach( $menu_datas as $row ){
+                    if( empty($row) ) continue;
+                
+                ?>
+
+                    <li class="nav-item">
+                        <a href="<?php echo $row['me_link']; ?>" target="_<?php echo $row['me_target']; ?>" ><?php echo $row['me_name'] ?></a>
+                        
+                    </li>
+                <?php
+                $i++;
+                }   //end foreach $row
+
+               ?>
+
+                </ul>
+
+            </div>
+
         </div>
     </nav>
+
+
 <!-- } 상단 끝 -->
 
 
 <hr>
 
 <!-- 콘텐츠 시작 { -->
-<div id="wrapper">
-    <div id="container_wr">
-   
-    <div id="container">
-        <?php if (!defined("_INDEX_")) { ?><h2 id="container_title"><span title="<?php echo get_text($g5['title']); ?>"><?php echo get_head_title($g5['title']); ?></span></h2><?php }
+    <div class="container_wr">
