a8bf0d3e62ce8949b7237e4d0b540b2de742f866
yeonji0 <kyj0279@gmail.com>
2022-09-06 17:01:06 +0900

    s

--- yeon/index.php
+++ yeon/index.php
@@ -41,23 +41,21 @@ include_once(G5_THEME_PATH.'/head.php');
 
     <div class="container sec01 rounded-5 mb-5 p-3">       
             <div class="row">   
-
-                <div class="col-md-4 col-lg-8 d-flex align-items-center">
+                <div class="col-md-6 col-lg-8 d-flex align-items-center">
                         <div class="txttWrap p-3">
                             <h2 class="mb-3">화상수업을 위해 개발된 <br> e-Classbook </h2>
                             <p>오프라인 교재를 그대로 올려서 수업하지 않습니다.<br>온라인 수업에 맞춰 개발된 온라인 전용 e-Classbook입니다.</p>
                         </div>
-                </div>      
-                
-                <div class="col-md-8 col-lg-4 p-3">
-                    <img class="img05" src="<? echo G5_THEME_IMG_URL ?>/main_visual_02.png" alt="">
+                </div>         
+                <div class="col-md-6 col-lg-4 p-3">
+                    <img class="img05 img-fluid" src="<? echo G5_THEME_IMG_URL ?>/main_visual_02.png" alt="">
                 </div>
 
             </div>
  
     </div>
         
-        
+       
     <div class="container sec02 p-3 mb-5">
         <div class="row">       
             <div class="col-md-6 d-flex position-relative ">
@@ -70,6 +68,23 @@ include_once(G5_THEME_PATH.'/head.php');
                     <h2 class="mb-3">지루할 틈이 없는 수업 진행</h2>
                     <p>온라인 교구와 액티비티로 흥미있는 수업이 진행됩니다.<br>선생님과 함께 직접 수업에 참여합니다.</p>
                 </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container sec03 p-3 rounded-5">
+        <img class="img08 position-absolute" src="<? echo G5_THEME_IMG_URL ?>/main_visual_bg02.png" alt="">
+        <div class="row">
+            <div class="col-md-6 position-relative d-flex">
+                <div class="txtWrap">
+                    <h2 class="mb-3">외국인 선생님 + <br> 한국인 선생님이 함께 </h2>
+                    <p>외국인 선생님과 수업할 때, 한국인 선생님도<br>옆에 함께합니다. 더 이상 영어가 두렵지 않습니다.</p>
+                </div>
+                <img class="img08 position-absolute" src="<? echo G5_THEME_IMG_URL ?>/main_visual_char.png" alt="">
+            </div>
+
+            <div class="col-md-6">
+             <img class="img09 " src="<? echo G5_THEME_IMG_URL ?>/main_visual_04.png" alt="">
             </div>
         </div>
     </div>
